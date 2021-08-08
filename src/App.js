08e5fbcbb8e217f6c5383642cde4a6b7ae7da5dc
Kristel Pau <77642513+kristelpau@users.noreply.github.com>
2021-08-08 15:51:37 +0300

    Added search form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Hello
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          Hello
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            Coded by Kristel Pau
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
