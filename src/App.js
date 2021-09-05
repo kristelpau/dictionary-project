9aa7da914d52327e47e2eb85f02698b3131ed776
Kristel Pau <77642513+kristelpau@users.noreply.github.com>
2021-09-05 23:02:04 +0300

    Added title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <header>
+          <h1 className="title">
+            Dictionary
+          </h1>
+        </header>
         <main>
           <Dictionary defaultKeyword="meditation" />
         </main>
